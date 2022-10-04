df2de8bd157ba2d8115fb7a4d744e2b3f5176031
ivc1-5 <aylidota@gmail.com>
2022-10-04 09:33:09 +0300

    problems 6,7

---
+++ src/Task1/problem6/problem6.java
@@ -0,0 +1,10 @@
+package Task1.problem6;
+
+public class problem6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++){
+            System.out.printf("%.2f", 1.0/i);
+            System.out.println();
+        }
+    }
+}
