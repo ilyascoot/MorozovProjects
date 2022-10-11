dd750b275d104975f06738fd408835d0fbd7c5bc
ivc1-5 <aylidota@gmail.com>
2022-10-11 11:27:53 +0300

    task2/problem4

--- src/task2/problem4/Tester.java
+++ src/task2/problem4/Tester.java
@@ -1,4 +1,40 @@
-package Task2.problem4;
+package task2.problem4;
+
+import java.util.Scanner;
 
 public class Tester {
+    public static void main(String[] args) {
+        String name;
+        String nameProc;
+        int memory;
+
+        Shop shop = new Shop();
+        Scanner in = new Scanner(System.in);
+
+        shop.addComp(new Computer("Windows", "Intel", 512));
+        shop.addComp(new Computer("Windows", "Intel", 1024));
+        shop.addComp(new Computer("Windows", "AMD", 512));
+        shop.addComp(new Computer("Windows", "AMD", 1024));
+
+        System.out.println("В магазине находятся компьютеры: ");
+        for(int i=0; i<shop.computers.size();i++)
+        {
+            System.out.println(shop.computers.get(i).toString());
+        }
+        Computer find;
+        System.out.println("Введите данные компьютера для его поиска и покупки");
+        name = in.nextLine();
+        nameProc = in.nextLine();
+        memory = in.nextInt();
+        find = shop.findPC(name,nameProc,memory);
+        if (find!=null)
+        {
+            System.out.println("Вы купили этот ПК, оставшиеся компьютеры в магазине: ");
+        }
+        shop.delComp(find);
+        for(int i=0; i<shop.computers.size();i++)
+        {
+            System.out.println(shop.computers.get(i).toString());
+        }
+    }
 }
